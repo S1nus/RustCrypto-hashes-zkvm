314a88e803f19a98c874f59de1753951b5a2e6c0
Connor O'Hara <connorohara@Connors-MacBook-Pro.local>
2024-04-22 15:49:20 -0400

    multivm

--- sha2/src/sha256.rs
+++ sha2/src/sha256.rs
@@ -2,8 +2,11 @@ use digest::{generic_array::GenericArray, typenum::U64};
 
 cfg_if::cfg_if! {
     if #[cfg(all(target_os = "zkvm", target_vendor = "succinct"))] {
-        mod succinct;
-        use succinct::compress;
+        mod sp1;
+        use sp1::compress;
+    } else if #[cfg(all(target_os = "zkvm", target_arch = "riscv32"))] {
+        mod risc0;
+        use risc0::compress;
     } else if #[cfg(feature = "force-soft")] {
         mod soft;
         use soft::compress;
